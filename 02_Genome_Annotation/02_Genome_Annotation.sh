d793465a5d28e0194b23f2cd2a172c18d349d796
antonkraege <59091892+antonkraege@users.noreply.github.com>
2024-10-08 15:03:16 +0200

    Update 02_Genome_Annotation.sh

--- 02_Genome_Annotation/02_Genome_Annotation.sh
+++ 02_Genome_Annotation/02_Genome_Annotation.sh
@@ -3,3 +3,12 @@
 #Mapping of RNAseq reads to the final genome assembly
 hisat2-build -p 80 Cm-478-11_final_PacBio_genome.m.fasta Cm-478-11_hisat2-index
 hisat2 --sensitive --summary-file ./ -p 80 -x Cm-478-11_21_hisat2-index -1 /DATA_RAID2/akraege/Hanna_Genome_Assembly/Cm-478-11/RNAseq/DE51NGSUKBR128750_Cm_S2_R1_001.fastq.gz -2 /DATA_RAID2/akraege/Hanna_Genome_Assembly/Cm-478-11/RNAseq/DE51NGSUKBR128750_Cm_S2_R2_001.fastq.gz -S RNAseq_to_Cm-478-11_21.sam
+
+#annotation of genome with braker
+braker.pl --species=Cm-478-11 --cores=80 --GENEMARK_PATH=/home/akraege/gmes_linux_64_4/ --genome=/DATA_RAID2/akraege/Hanna_Genome_Assembly/Cm-478-11/genome-manual2/Cm_478-11_final_genome_m_21.fasta --bam ./mapped_reads/RNAseq_to_Cm-478-11_21.bam
+
+#quality control of braker annotation with BUSCO
+busco -i ../Braker2/braker/braker_aa.fasta -o Cm-478-11_final_Pacbio-m -l chlorophyta -c 40 -m prot
+
+#annotation of secreated proteins with signalp6
+signalp6 --mode slow-sequential --output_dir Signalp6/ --fastafile RNAseq/braker/Celongata/braker.aa
